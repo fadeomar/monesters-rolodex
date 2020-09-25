df5bfc1ebfc4b9120719f3c71e0e567cede48606
fadeomar <fadeomar2015@gmail.com>
2020-09-26 02:49:55 +0300

    Updates

---
+++ precache-manifest.5356d48ebd1acfae1fe3a762bfb3a857.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7756de154316d15614c8bc04e3cb54",
+    "url": "/monesters-rolodex/index.html"
+  },
+  {
+    "revision": "4fa1588ec9eeb08542d2",
+    "url": "/monesters-rolodex/static/css/main.c5ca2b36.chunk.css"
+  },
+  {
+    "revision": "b0732ffd9107e85264a9",
+    "url": "/monesters-rolodex/static/js/2.ae38f786.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monesters-rolodex/static/js/2.ae38f786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa1588ec9eeb08542d2",
+    "url": "/monesters-rolodex/static/js/main.eb44559b.chunk.js"
+  },
+  {
+    "revision": "39e4cffe10a714e359aa",
+    "url": "/monesters-rolodex/static/js/runtime-main.04c173e7.js"
+  }
+]);
