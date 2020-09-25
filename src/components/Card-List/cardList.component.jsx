f9aa898ceda271fceec3829f4ea4cdbd57e170f4
fadeomar <fadeomar2015@gmail.com>
2020-09-26 00:26:25 +0300

    fix eslint configration

--- src/components/Card-List/cardList.component.jsx
+++ src/components/Card-List/cardList.component.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import Card from '../Card';
-import './card-list.styles.css'
+import React from "react";
+import Card from "../Card";
+import "./card-list.styles.css";
 
 const CardList = (props) => {
-    return (
+  return (
     <div className="card-list">
-              {props.monsters.map(monster => (
-                  <Card key={monster.id} monster={monster} />
+      {props.monsters.map((monster) => (
+        <Card key={monster.id} monster={monster} />
       ))}
     </div>
-    )
-}
+  );
+};
 
 export default CardList;
